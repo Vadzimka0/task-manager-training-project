b298dad6a226b23d74cd44359da78b473a967812
Vadzim Dzianisik <wadik.nhl@gmail.com>
2022-08-28 01:23:46 +0300

    jwt auth guard handle error

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export default class JwtAuthGuard extends AuthGuard('jwt') {}
+export default class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
+  }
+
+  handleRequest(err, user, info) {
+    if (err || !user) {
+      throw err || new UnauthorizedException('Token is not valid or has expired');
+    }
+    return user;
+  }
+}
