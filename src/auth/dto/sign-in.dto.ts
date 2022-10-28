512390fd30cb97a65eefb01e7c3deb83adacd40d
Vadzim Dzianisik <wadik.nhl@gmail.com>
2022-10-28 08:46:01 +0300

    fix: signInDto

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class SignInDto {
   @ApiProperty({ example: 'rogerfederer@gmail.com' })
+  @IsNotEmpty()
   readonly email: string;
 
   @ApiProperty({ example: 'Y29nbml0ZXE=' })
+  @IsNotEmpty()
   readonly password: string;
 }
